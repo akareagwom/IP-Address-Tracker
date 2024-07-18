3a1e9cafb1710ab2ff74cd639f13113f43d0d956
akareagwom <akareazikang20@gmail.com>
2024-07-18 14:58:38 +0100

    first commit

---
+++ ip-address-tracker/src/app/components/heading.tsx
@@ -0,0 +1,10 @@
+export const Heading =()=>{
+    return(
+        <>
+        <div className="p-0 m-0 ">
+            {/* <h1 className="text-yellow-200">hello</h1> */}
+            <img className="w-[100%]" src="/images/pattern-bg-desktop.png" alt="asset"  />
+        </div>
+        </>
+    )
+}
---
+++ ip-address-tracker/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Heading } from "./components/heading";
+
+export default function Home() {
+  return (
+    <>
+    <Heading/>
+    </>
+  );
+}
