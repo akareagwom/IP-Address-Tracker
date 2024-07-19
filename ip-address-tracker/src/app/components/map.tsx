b3a82f5ecdfcfd3b9d1eeaf1a82e57145f7a6689
akareagwom <akareazikang20@gmail.com>
2024-07-19 08:56:50 +0100

    leaflet map

--- ip-address-tracker/src/app/components/map.tsx
+++ ip-address-tracker/src/app/components/map.tsx
@@ -1,34 +1,38 @@
 "use client"
 import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression, LatLngTuple } from 'leaflet';
+
+import dynamic from 'next/dynamic';
 import L from 'leaflet';
+import { posix } from "path";
+
+// Fix default icon issue
+// delete L.Icon.Default.prototype._getIconUrl;
+
+
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: 'leaflet/dist/images/marker-icon-2x.png',
+//   iconUrl: 'leaflet/dist/images/marker-icon.png',
+//   shadowUrl: 'leaflet/dist/images/marker-shadow.png',
+// });
 
  const Mapper =()=>{
-    const mapRef = useRef<L.Map | null>(null);
 
-    useEffect(() => {
-      // Check if the map has already been initialized
-      if (!mapRef.current) {
-        mapRef.current = L.map('map').setView([51.505, -0.09], 13);
-  
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(mapRef.current);
-  
-        L.marker([51.505, -0.09]).addTo(mapRef.current)
-          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-          .openPopup();
-      }
-  
-      // Clean up the map instance on component unmount
-      return () => {
-        if (mapRef.current) {
-          mapRef.current.remove();
-          mapRef.current = null;
-        }
-      };
-    }, []);
     return(
-        <div id="map" className="h-[60vh] w-[100%]"></div>
+      <div className="">
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     )
 }
 
--- ip-address-tracker/src/app/page.tsx
+++ ip-address-tracker/src/app/page.tsx
@@ -2,14 +2,26 @@
 import Image from "next/image";
 import { Heading } from "./components/heading";
 import Mapper from "./components/map";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 // import { Mapper } from "./components/map";
 
 export default function Home() {
+  const Map = useMemo(() => dynamic(
+    () => import('@/app/components/map'),
+    {
+        loading: () => <p>A map is loading</p>,
+        ssr: false
+    }
+), [])
   return (
     <>
     <Heading/>
     {/* <Mappper/> */}
-    <Mapper/>
+    <div>            
+      {/* <Mapper/> */}
+      <Map/>
+    </div>
     </>
   );
 }
