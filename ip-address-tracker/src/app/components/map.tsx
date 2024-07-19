56485926d2a43aa969983df195443e27534d8f0d
akareagwom <akareazikang20@gmail.com>
2024-07-19 09:53:40 +0100

    leaflet deleted

--- ip-address-tracker/src/app/components/map.tsx
+++ ip-address-tracker/src/app/components/map.tsx
@@ -1,39 +1,10 @@
 "use client"
-import { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression, LatLngTuple } from 'leaflet';
+;
 
-import dynamic from 'next/dynamic';
-import L from 'leaflet';
-import { posix } from "path";
+const Map = () => {
+  return (
+<div className=""></div>
+  );
+};
 
-// Fix default icon issue
-// delete L.Icon.Default.prototype._getIconUrl;
-
-
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: 'leaflet/dist/images/marker-icon-2x.png',
-//   iconUrl: 'leaflet/dist/images/marker-icon.png',
-//   shadowUrl: 'leaflet/dist/images/marker-shadow.png',
-// });
-
- const Mapper =()=>{
-
-    return(
-      <div className="">
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
-    )
-}
-
-export default Mapper;
+export default Map;
--- ip-address-tracker/src/app/page.tsx
+++ ip-address-tracker/src/app/page.tsx
@@ -1,25 +1,33 @@
 "use client"
 import Image from "next/image";
 import { Heading } from "./components/heading";
-import Mapper from "./components/map";
+// import Map from "./components/map";
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
+import Head from 'next/head';
+import Map from "./components/map";
 // import { Mapper } from "./components/map";
 
 export default function Home() {
-  const Map = useMemo(() => dynamic(
-    () => import('@/app/components/map'),
-    {
-        loading: () => <p>A map is loading</p>,
-        ssr: false
-    }
-), [])
+//   const Map = useMemo(() => dynamic(
+//     () => import('@/app/components/map'),
+//     {
+//         loading: () => <p>A map is loading</p>,
+//         ssr: false
+//     }
+// ), [])
   return (
     <>
+      <Head>
+        <title>map</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet/dist/leaflet.css"
+        />
+      </Head>
     <Heading/>
-    {/* <Mappper/> */}
-    <div>            
-      {/* <Mapper/> */}
+    <div>              
       <Map/>
     </div>
     </>
