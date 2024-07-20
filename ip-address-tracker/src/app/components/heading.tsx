8bbfddee9ee737d73b922c8f4acefa289a4f008f
akareagwom <akareazikang20@gmail.com>
2024-07-20 21:30:55 +0100

    search mobile

--- ip-address-tracker/src/app/components/heading.tsx
+++ ip-address-tracker/src/app/components/heading.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { IoIosArrowForward } from "react-icons/io";
 
 export const Heading =()=>{
     return(
@@ -7,8 +8,10 @@ export const Heading =()=>{
             <h1 className="text-white text-xl mt-4">IP Address Tracker</h1>
             <input
             className="
-            p-2
-            w-[34%]
+            lg:p-2
+            p-3
+            lg:w-[34%]
+            w-[75%]
             mt-4
             text-[13px]
             rounded-[8px]
@@ -17,7 +20,9 @@ export const Heading =()=>{
              name="" 
              placeholder="Search for any IP address or domain" 
              id="" />
-             <button className="text-white absolute rounded-l-none bg-black py-1.5 right-[356px] px-4 rounded-[8px] top-[60px]">x</button>
+             <button className="text-white absolute rounded-l-none bg-black py-3.5 lg:py-1.5 right-[40px] lg:right-[356px] px-4 rounded-[8px] top-[60px]">
+             <IoIosArrowForward />
+             </button>
         </div>
         </>
     )
