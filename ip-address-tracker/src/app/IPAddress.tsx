8ab5a4d7885b454ba095d22ec2aad0e8754e395e
akareagwom <akareazikang20@gmail.com>
2024-07-24 13:54:06 +0100

    IP APIdata

--- ip-address-tracker/src/app/IPAddress.tsx
+++ ip-address-tracker/src/app/IPAddress.tsx
@@ -1,2 +1,10 @@
 const getUrl = 'https://geo.ipify.org/api/v2/country?apiKey=at_xCF6qmBPCBmGpMJnzxcuH8c0943yj&ipAddress=8.8.8.8';
 
+interface Info {
+    ipAddress: string;
+    location : object;
+}
+
+const fetchData =()=>{
+    
+}
