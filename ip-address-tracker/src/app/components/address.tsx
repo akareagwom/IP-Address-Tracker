d758be1a06ad95c904e3c644f40a5b1858ce4de6
akareagwom <akareazikang20@gmail.com>
2024-07-25 20:19:32 +0100

    setdata

--- ip-address-tracker/src/app/components/address.tsx
+++ ip-address-tracker/src/app/components/address.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import {fetchData} from '../IPAddress';
 
+interface DataItem {
+    ipAddress: string;
+    location: object;
+  }
+
 const Address=() =>{
+    const [data, setData] = useState<DataItem[]>([]);
+    const [ipAddress, setIpAddress] = useState<string>("");
+
+    const DataFunction =()=>{
+        setData(data);
+        setIpAddress(ipAddress);
+        fetchData();
+    }
 
     return(
         <div className=" lg:flex bg-white shadow-xl justify-between absolute top-[25%] lg:top-[28%] left-[10%] lg:left-[10%] rounded-[10px] p-12 w-[80%]">
+            
+            {}
             <div className="border-[1px solid red]">
                 <h1 className="text-[8px]" >IP ADDRESS</h1>
                 <p>brrrrrrrrr</p>
