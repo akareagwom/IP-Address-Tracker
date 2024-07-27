6cb908dd91660d87fa36e5386b1bb91b9bacec62
akareagwom <akareazikang20@gmail.com>
2024-07-27 22:56:38 +0100

    setdata2 map

--- ip-address-tracker/src/app/components/address.tsx
+++ ip-address-tracker/src/app/components/address.tsx
@@ -19,11 +19,14 @@ const Address=() => {
     return(
         <div className=" lg:flex bg-white shadow-xl justify-between absolute top-[25%] lg:top-[28%] left-[10%] lg:left-[10%] rounded-[10px] p-12 w-[80%]">
             
-            {}
+            {/* {data.map(()=>{
+
             <div className="border-[1px solid red]">
                 <h1 className="text-[8px]" >IP ADDRESS</h1>
                 <p>brrrrrrrrr</p>
             </div>
+            
+            })} */}
             <div className="border-[1px solid red]">
                 <h1 className="text-[8px]" >LOCATION</h1>
                 <p>brrrrrrrrr</p>
