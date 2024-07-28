ffd1030a3f960dcf37af25e206d5a8cb4b216516
akareagwom <akareazikang20@gmail.com>
2024-07-28 20:00:43 +0100

    returns location object

--- ip-address-tracker/src/app/components/address.tsx
+++ ip-address-tracker/src/app/components/address.tsx
@@ -4,6 +4,8 @@ import {fetchData} from '../IPAddress';
 interface DataItem {
     ipAddress: string;
     location: object;
+    timezone: string;
+    isp: string;
   }
 
 const Address=() => {
@@ -31,25 +33,30 @@ const Address=() => {
         <div className=" lg:flex bg-white shadow-xl justify-between absolute top-[25%] lg:top-[28%] left-[10%] lg:left-[10%] rounded-[10px] p-12 w-[80%]">
             
             {data.map((item=>
-                <div key={item.ipAddress} className="border-[1px solid red]">
+            <div key={item.ipAddress} className="lg:flex">
+                <div  className="border-[1px solid red]">
                     <h1 className="text-[8px]" >IP ADDRESS</h1>
                     <p>{item.ipAddress}</p>
                 </div>
 
+                <div className="border-[1px solid red]">
+                    <h1 className="text-[8px]" >LOCATION</h1>
+                    <p>{JSON.stringify(item.location)}</p>
+                </div>
+
+                <div className="border-[1px solid red]">
+                    <h1 className="text-[8px]" >TIMEZONE</h1>
+                    <p>brrrrrrrrr</p>
+                </div>
+                <div className="">
+                    <h1 className="text-[8px]" >ISP</h1>
+                    <p>brrrrrrrrr</p>
+                </div>
+
+            </div>
+
             ))}
             
-            <div className="border-[1px solid red]">
-                <h1 className="text-[8px]" >LOCATION</h1>
-                <p>brrrrrrrrr</p>
-            </div>
-            <div className="border-[1px solid red]">
-                <h1 className="text-[8px]" >TIMEZONE</h1>
-                <p>brrrrrrrrr</p>
-            </div>
-            <div className="">
-                <h1 className="text-[8px]" >ISP</h1>
-                <p>brrrrrrrrr</p>
-            </div>
         </div>
     )
 }
