1dd6654f9877f524618e8f5181b1d35a632ba560
akareagwom <akareazikang20@gmail.com>
2024-08-02 15:03:55 +0100

    ipaddress search

--- src/app/components/heading.tsx
+++ src/app/components/heading.tsx
@@ -5,11 +5,18 @@ import { useEffect, useState } from 'react';
 
 export const Heading =()=>{
     const [ipAddress, setIpAddress] = useState<string>("");
+
+
+    const handleSubmit = (e:React.FormEvent)=>{
+        e.preventDefault();
+        fetchData();
+    }
+
     return(
         <>
         <div className="p-0 m-0 h-[40vh] flex flex-col items-center bg-[url('/images/pattern-bg-mobile.png')] lg:bg-[url('/images/pattern-bg-desktop.png')] ">
             <h1 className="text-white text-xl mt-4">IP Address Tracker</h1>
-            <div className="relative">
+            <form onSubmit={handleSubmit} className="relative">
                 <input
                 className="
                 lg:p-2
@@ -20,14 +27,16 @@ export const Heading =()=>{
                 text-[13px]
                 rounded-[8px]
                 "
-                type="search" 
+                type="search"
+                value={ipAddress} 
+                onChange={(e) => setIpAddress(e.target.value)}
                 name="" 
                 placeholder="Search for any IP address or domain" 
                 id="" />
-                <button className="text-white absolute rounded-l-none bg-black py-3.5 lg:py-2.5 right-[40px] lg:right-[0px] px-4 rounded-[8px] top-[15px]">
+                <button type="submit" className="text-white absolute rounded-l-none bg-black py-3.5 lg:py-2.5 right-[40px] lg:right-[0px] px-4 rounded-[8px] top-[15px]">
                 <IoIosArrowForward />
                 </button>
-            </div>
+            </form>
         </div>
         </>
     )
