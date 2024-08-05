ab960abfe031f216fc9feca78c338da02ef40cb2
akareagwom <akareazikang20@gmail.com>
2024-08-05 16:32:44 +0100

    search content

--- src/app/components/heading.tsx
+++ src/app/components/heading.tsx
@@ -15,14 +15,14 @@ export const Heading =()=>{
     return(
         <>
         <div className="p-0 m-0 h-[40vh] flex flex-col items-center bg-[url('/images/pattern-bg-mobile.png')] lg:bg-[url('/images/pattern-bg-desktop.png')] ">
-            <h1 className="text-white text-xl mt-4">IP Address Tracker</h1>
+            <h1 className="text-white text-xl mt-[20%] lg:mt-4">IP Address Tracker</h1>
             <form onSubmit={handleSubmit} className="relative">
                 <input
                 className="
                 lg:p-2
                 p-3
                 lg:w-[350px]
-                w-[75%]
+                w-[300px]
                 mt-4
                 text-[13px]
                 rounded-[9px]
@@ -33,7 +33,7 @@ export const Heading =()=>{
                 name="" 
                 placeholder="Search for any IP address or domain" 
                 id="" />
-                <button type="submit" className="text-white absolute rounded-l-none bg-black py-3.5 lg:py-2.5 right-[40px] lg:right-[0px] px-4 rounded-[8px] top-[15px]">
+                <button type="submit" className="text-white absolute rounded-l-none bg-black py-4 lg:py-3 right-[0px] lg:right-[0px] px-4 rounded-[8px] top-[15px]">
                 <IoIosArrowForward />
                 </button>
             </form>
